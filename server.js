ef8298ef4c76328641716acdf69a87fe160e1e2c
Otacosdelyon <Otacosdelyon@users.noreply.github.com>
2026-09-25 22:17:50 +0100

    Fix duplicate path declaration

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
 
 const app = express();
 const PORT = 3000;
